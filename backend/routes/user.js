c448e2455e73dd7676b1059850b349e0dacb06e4
nbvishwa <vishwavijet.bagoji@tarento.com>
2018-11-20 10:11:52 +0530

    package changes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require("../models/user");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
